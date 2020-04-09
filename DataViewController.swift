ac9a30f5dd58c5f1755f2254dc2b2315c132f0f5
Anthony Terrano <61809326+Terranova-Python@users.noreply.github.com>
2020-04-09 13:46:27 -0400

    Files Upload

---
+++ DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  Terra Tech Blog
+//
+//  Created by Anthony Terrano on 12/30/19.
+//  Copyright © 2019 Terranova Technologies. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
